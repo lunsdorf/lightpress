e8985ab4369bb891a6a59ad74e46c7e08389b69f
Sören Lünsdorf <code@lunsdorf.com>
2016-09-21 00:06:00 +0200

    export types/interfaces in package main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,12 @@
+// Export TypeScript Types
+export * from "./HttpHeaders";
+
+// Export TypeScript Interfaces
+export * from "./IHttpHandler";
+export * from "./IHttpResult";
+export * from "./IHttpRequest";
+
+// Export Library Implementation
 export * from "./MIME_TYPES";
 export {default as HttpError} from "./HttpError";
 export {default as HttpServer} from "./HttpServer";
