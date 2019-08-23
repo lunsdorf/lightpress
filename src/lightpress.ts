330c396d043d65110e5ce714f4597a6a33c7bfd4
Sören Lünsdorf <mail@lunsdorf.com>
2019-08-23 18:47:37 +0200

    feat: provide parsed URL in request info

--- src/lightpress.ts
+++ src/lightpress.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { parse } from "url";
 import { LightpressHandler } from "./types/lightpress-handler";
 import { LightpressInfo } from "./types/lightpress-info";
 import { LightpressResult } from "./types/lightpress-result";
@@ -14,12 +15,17 @@ export function lightpress<T extends LightpressInfo = LightpressInfo>(
   handler: LightpressHandler,
   options: LightpressOptions<T> = {}
 ): (request: IncomingMessage, response: ServerResponse) => void {
+  if ("function" !== typeof handler) {
+    throw new TypeError("handler must be a function");
+  }
+
   const sendError = options.sendError || defaultSendError;
   const sendResult = options.sendResult || defaultSendResult;
 
   return (request: IncomingMessage, response: ServerResponse) => {
     const timestamp = Date.now();
-    const info = { timestamp, request } as T;
+    const url = parse(request.url || "/", true)
+    const info = { request, timestamp, url } as T;
 
     new Promise<LightpressResult>(resolve => resolve(handler(info)))
       .then(result => sendResult(response, info, result))
