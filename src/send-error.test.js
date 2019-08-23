5a0d2850175ae98ae4ecfec463cf811602a989b2
Sören Lünsdorf <mail@lunsdorf.com>
2019-08-23 19:00:37 +0200

    test: send error callback

---
+++ src/send-error.test.js
@@ -0,0 +1,20 @@
+jest.mock('./send-result');
+
+const { sendError } = require('./send-error');
+const { sendResult } = require('./send-result');
+const { HttpError } = require('./http-error');
+
+describe("sendError", () => {
+  beforeEach(() => jest.resetAllMocks());
+
+  it("delegates to `resultHandler`", () => {
+    const responseFixture = {};
+    const infoFixture = {};
+    const codeFixture = 400;
+
+    sendError(responseFixture, infoFixture, new HttpError(400));
+
+    expect(sendResult).toHaveBeenCalledTimes(1);
+    expect(sendResult).toHaveBeenCalledWith(responseFixture, infoFixture, { statusCode: codeFixture });
+  });
+});
