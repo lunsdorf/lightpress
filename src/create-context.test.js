1989bc28cbf2f6d6f709275dc1492743fad782d9
lunsdorf <code@lunsdorf.com>
2020-04-03 18:39:31 +0200

    refactor: remove URL from context object

--- src/create-context.test.js
+++ src/create-context.test.js
@@ -1,24 +1,13 @@
-jest.mock("url");
-
-const { parse } = require("url");
 const { createContext } = require("./create-context");
 
 describe("createContext", () => {
   beforeEach(() => jest.resetAllMocks());
 
   it("creates context object", () => {
-    const urlFixture = "/some/url";
-    const parsedUrlFixture = {};
-    const requestFixture = { url: urlFixture };
-
-    parse.mockImplementation(() => parsedUrlFixture);
+    const requestFixture = {  };
 
     expect(createContext(requestFixture)).toEqual({
       request: requestFixture,
-      url: parsedUrlFixture,
     });
-
-    expect(parse).toHaveBeenCalledTimes(1);
-    expect(parse).toHaveBeenCalledWith(urlFixture, true);
   });
 });
--- src/types/lightpress-context.ts
+++ src/types/lightpress-context.ts
@@ -1,10 +1,6 @@
 import { IncomingMessage } from "http";
-import { UrlWithParsedQuery } from "url";
 
 export type LightpressContext = {
   /** The incoming server request. */
   request: IncomingMessage;
-
-  /** The request's parsed URL. */
-  url: UrlWithParsedQuery
 };
