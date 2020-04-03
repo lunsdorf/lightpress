855678d60cfd19cb4678cb62d965f91edc9432e6
lunsdorf <code@lunsdorf.com>
2020-04-03 18:43:37 +0200

    chore: take rollup output paths from

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,18 @@
 const importResolver = require("rollup-plugin-import-resolver");
 const babel = require("rollup-plugin-babel");
+const pkg = require("./package.json");
 
 const FILE_EXTENSIONS = [".ts", "js"];
 
 module.exports = {
   input: "src/index.ts",
   output: [
-    { file: "lib/index.mjs", format: "esm" },
-    { file: "lib/index.js", format: "cjs" },
+    { file: pkg.module, format: "esm" },
+    { file: pkg.main, format: "cjs", exports: "named" },
   ],
+  external: require("module").builtinModules,
   plugins: [
     importResolver({ extensions: FILE_EXTENSIONS }),
     babel({ extensions: FILE_EXTENSIONS }),
-  ]
+  ],
 };
