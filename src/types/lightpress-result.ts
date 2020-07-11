0b414e18105ae467bfef6038a0e5435c9ab9b395
Sören Lünsdorf <mail@lunsdorf.com>
2020-07-11 10:12:45 +0200

    feat: more usable result object typings

--- src/types/lightpress-result.ts
+++ src/types/lightpress-result.ts
@@ -3,11 +3,11 @@ import { Readable } from "stream";
 
 export type LightpressResult = void | null | {
   /** Optional response status code (defaults to `200`). */
-  statusCode?: number;
+  statusCode?: null | number;
 
   /** Optional response payload. */
   body?: null | string | Buffer | Readable;
 
   /** Optional HTTP response headers. */
-  headers?: OutgoingHttpHeaders;
+  headers?: null | OutgoingHttpHeaders;
 };
