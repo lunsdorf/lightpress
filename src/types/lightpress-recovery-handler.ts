55b55116c58482adad181fda7fa14a83dc00c45f
Sören Lünsdorf <mail@lunsdorf.com>
2021-01-16 18:37:25 +0100

    refactor: pass request to error recovery handler

--- src/types/lightpress-recovery-handler.ts
+++ src/types/lightpress-recovery-handler.ts
@@ -1,5 +1,7 @@
+import { IncomingMessage } from "http";
 import { LightpressResult } from "./lightpress-result";
 
 export type LightpressRecoveryHandler = (
+  request: IncomingMessage,
   error: Error
 ) => LightpressResult | Promise<LightpressResult>;
