5b5229e4d8c7114f7fff382b3abfc9d4e419384a
Lunsdorf <975967+lunsdorf@users.noreply.github.com>
2026-01-18 17:45:12 +0100

    use editorconfig in biome

--- src/send-result.ts
+++ src/send-result.ts
@@ -4,17 +4,17 @@ import { isReadableStream } from "./is-readable-stream";
 
 /** Passes the given {@link HttpResult} to the given {@link ServerResponse}. */
 export function sendResult(response: ServerResponse, result: HttpResult): void {
-  const statusCode = result?.statusCode ?? 200;
+	const statusCode = result?.statusCode ?? 200;
 
-  if (result?.headers) {
-    response.writeHead(statusCode, result.headers);
-  } else {
-    response.statusCode = statusCode;
-  }
+	if (result?.headers) {
+		response.writeHead(statusCode, result.headers);
+	} else {
+		response.statusCode = statusCode;
+	}
 
-  if (isReadableStream(result?.body)) {
-    result.body.pipe(response);
-  } else {
-    response.end(result?.body ?? null);
-  }
+	if (isReadableStream(result?.body)) {
+		result.body.pipe(response);
+	} else {
+		response.end(result?.body ?? null);
+	}
 }
